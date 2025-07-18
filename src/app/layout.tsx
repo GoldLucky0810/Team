bc6301376228ca89e5644f4fe13b9f90260f2fbf
xiaojin <108878106+keremeris@users.noreply.github.com>
2025-07-18 19:31:06 +0900

    Change script type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Script from 'next/script'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -38,16 +39,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
      
         <a href="#" id="scroll-top" className="scroll-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
         {/* Vendor JS */}
-        <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/vendor/php-email-form/validate.js"></script>
-        <script src="/assets/vendor/aos/aos.js"></script>
-        <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-        <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/php-email-form/validate.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/aos/aos.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/swiper/swiper-bundle.min.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/glightbox/js/glightbox.min.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js" strategy="beforeInteractive"/>
+        <Script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js" strategy="beforeInteractive"/>
 
         {/* Main JS */}
-        <script src="/assets/js/main.js"></script>
+        <Script src="/assets/js/main.js" strategy="beforeInteractive"/>
       </body>
     </html>
   )
